880765c1678c601ca78265774c4025feb0ff248f
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-04-05 22:46:59 -0300

    Atualizando projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import jsxIMG from './assets/jsx.png';
+import './styles.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <div className="container-login">
+
+        <div className="wrap-login">
+
+          <form className="login-form">
+
+            <span className="login-form-title">Sign In!</span>
+            <span className="login-form-title">
+              <img src={jsxIMG} alt="React Icon" />
+            </span>
+
+            <div className="wrap-input">
+              <input className={email !== "" ? "has-val input" : "input"} // Condição adicionada para verificar se o email está preenchido
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder='Email'></span>
+            </div>
+
+            <div className="wrap-input">
+              <input className={password !== "" ? "has-val input" : "input"} // Condição adicionada para verificar se o password está preenchido
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder='Password'></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Sign In</button>
+            </div>
+
+            <div className="text-center">
+              <span className="text1">Don't have account?</span>
+            </div>
+            <div className='text-center'>
+              <a href="#" className="text2">Create account!</a>
+            </div>
+
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
